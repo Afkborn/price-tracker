9a89c0b8ad4235728cc0b8d59ec52b62e1ee9b03
Bilgehan K <kalaybilgehan60@gmail.com>
2021-09-30 23:16:14 +0300

    Tracker python dosyası içindeki bazı değişkenler private yapıldı. createDb adında  private fonksiyon yazıldı ve checkDb daha kısaltıldı. getIdList adında fonksiyon yazıldı. loadDb çalışırken stocks and prices dbsini kontrol etme eklendi loadDb bool değer eklerken string ekliyordu boolean türüne çevrildi daha bir çok hata giderildi daha sonra çözmek üzere hata eklendi :)

--- Python/Tracker.py
+++ Python/Tracker.py
@@ -3,22 +3,43 @@ from os.path import exists
 import sqlite3 as sql
 from Python.Product import Product
 from time import time
+
 CREATETABLEPRODUCT = """CREATE TABLE IF NOT EXISTS products (id	INTEGER PRIMARY KEY,productName TEXT NOT NULL,productURL TEXT NOT NULL,productTime INTEGER NOT NULL,checkStock TEXT NOT NULL, checkPrice TEXT NOT NULL,checkTime INTEGER NOT NULL)"""
 CREATETABLEPRICES = """CREATE TABLE IF NOT EXISTS prices (id INTEGER NOT NULL, priceTime INTEGER NOT NULL, price INTEGER NOT NULL)"""
 CREATETABLESTOCKS = """CREATE TABLE IF NOT EXISTS stocks (id INTEGER NOT NULL, stockTime INTEGER NOT NULL, stock TEXT NOT NULL)"""
+
 class PriceTracker:
-    isLoadDB = False
-    dbName = "database.db"
-    products = []
-    dbLen = 0
-    dbLoc = f"{getcwd()}\Database\{dbName}"
+
+    __isLoadDB = False
+    __dbName = "database.db"
+    __products = []
+    __dbLen = 0
+    __dbLoc = f"{getcwd()}\Database\{__dbName}"
+
     def __init__(self) -> None:
         self.checkDB()
 
+    def getDbName(self) -> str:
+        return self.__dbName
+    def getIsLoadDb(self) -> bool:
+        return self.__isLoadDB
+    def getDbLen(self):
+        self.checkDB()
+        return self.__dbLen
+    def getProductsList(self) -> list:
+        self.checkDB()
+        return self.__products
+
+
+    def checkDB(self):
+        if exists(self.__dbLoc):
+            self.__loadDB()
+        else:
+            self.__createDB
 
     def __loadDB(self):
-        self.products = []
-        self.db = sql.connect(self.dbLoc)
+        self.__products = []
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute("SELECT name FROM sqlite_master")
         tableNames = self.im.fetchall()
@@ -32,33 +53,43 @@ class PriceTracker:
             allDb = self.im.fetchall()
             for i in allDb:
                 id, productName, productURL, productTime, checkStock, checkPrice, checkTime = i
-                newProduct = Product(id,productName,productURL,productTime,checkStock,checkPrice,checkTime)
-                self.products.append(newProduct)
-            self.isLoadDB = True
+                if checkStock == "True":
+                    checkStock = True
+                else:
+                    checkStock = False
+                if checkPrice == "True":
+                    checkPrice = True
+                else:
+                    checkPrice = False
+                findProduct = Product(id,productName,productURL,productTime,checkStock,checkPrice,checkTime)
+                if checkPrice:
+                    self.getLastPriceWithProductFromDb(findProduct)
+                if checkStock:
+                    self.getLastStockStateWithProductFromDb(findProduct)
+                self.__products.append(findProduct)
+            self.__isLoadDB = True
         else:
             self.im.execute(CREATETABLEPRODUCT)
             self.im.execute(CREATETABLEPRICES)
             self.im.execute(CREATETABLESTOCKS)
-            self.isLoadDB = True
+            self.__isLoadDB = True
         self.db.close()
         self.getProductLenFromDB()
 
-    def checkDB(self):
-        if exists(self.dbLoc):
-            self.__loadDB()
-        else:
-            self.db = sql.connect(self.dbLoc)
-            self.im = self.db.cursor()
-            self.im.execute(CREATETABLEPRODUCT)
-            self.im.execute(CREATETABLEPRICES)
-            self.im.execute(CREATETABLESTOCKS)
-            self.isLoadDB = True
-            self.db.close()
-            self.dbLen = 0
+    def __createDB(self):
+        self.db = sql.connect(self.__dbLoc)
+        self.im = self.db.cursor()
+        self.im.execute(CREATETABLEPRODUCT)
+        self.im.execute(CREATETABLEPRICES)
+        self.im.execute(CREATETABLESTOCKS)
+        self.__isLoadDB = True
+        self.db.close()
+        self.__dbLen = 0
+
 
     def addProduct(self,product:Product) ->int:
         """Return last insert row id as int."""
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(f"INSERT INTO products(productName,productURL,productTime,checkStock,checkPrice,checkTime) VALUES('{product.getProductName()}','{product.getProductURL()}',{product.getProductTime()},'{product.getCheckStock()}','{product.getCheckPrice()}',{product.getCheckTime()})")
         self.db.commit()
@@ -66,12 +97,10 @@ class PriceTracker:
         lastInsertID = self.getProductFromDbWithProductTime(product.getProductTime()).getId()
         return lastInsertID
     
-
-
     def addPriceToDb(self,product:Product, price : float):
         product.setPrice(price)
         product.setPriceTime(time())
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(f"INSERT INTO prices(id,priceTime,price) VALUES('{product.getId()}','{product.getPriceTime()}','{product.getPrice()}')")
         self.db.commit()
@@ -80,7 +109,7 @@ class PriceTracker:
     def addStockToDb(self,product:Product, stock : bool):
         product.setStock(stock)
         product.setStockTime(time())
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(f"INSERT INTO stocks(id,stockTime,stock) VALUES('{product.getId()}','{product.getStockTime()}','{product.getStock()}')")
         self.db.commit()
@@ -88,7 +117,7 @@ class PriceTracker:
 
     def deleteProduct(self,product:Product) -> bool:
         productId = product.getId()
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(f"DELETE FROM products WHERE id = '{productId}'")
         self.db.commit()
@@ -96,7 +125,7 @@ class PriceTracker:
     
     def getProductFromDbWithId(self,id) -> Product:
         """ if find return Product or none"""
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(f"SELECT * FROM products WHERE id = '{id}'")
         findProduct = self.im.fetchone()
@@ -123,7 +152,7 @@ class PriceTracker:
         return findProduct
     
     def getLastPriceWithProductFromDb(self, product:Product)-> float:
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(F"SELECT MAX(priceTime),price FROM prices WHERE id = '{product.getId()}'")
         priceTime, price = self.im.fetchone()
@@ -135,7 +164,7 @@ class PriceTracker:
     
 
     def getLastStockStateWithProductFromDb(self,product:Product) -> bool:
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(F"SELECT MAX(stockTime),stock FROM stocks WHERE id = '{product.getId()}'")
         stockTime, stock = self.im.fetchone()
@@ -152,7 +181,7 @@ class PriceTracker:
 
     def getProductFromDbWithProductTime(self,productTime):
         """ if find return Product or none"""
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute(f"SELECT * FROM products WHERE productTime = '{productTime}'")
         findProduct = self.im.fetchone()
@@ -164,26 +193,25 @@ class PriceTracker:
         return findProduct 
         
     def getProductLenFromDB(self) -> int:
-        self.db = sql.connect(self.dbLoc)
+        self.db = sql.connect(self.__dbLoc)
         self.im = self.db.cursor()
         self.im.execute("select last_insert_rowid() from products")
-        self.dbLen = len(self.im.fetchall())
+        self.__dbLen = len(self.im.fetchall())
         self.db.close() 
-        return self.dbLen
+        return self.__dbLen
 
-    def getDbLen(self):
-        return self.dbLen
+    def getIdListFromDb(self) -> list:
+        self.db = sql.connect(self.__dbLoc)
+        self.im = self.db.cursor()
+        self.im.execute("select id from products")
+        idListe = []
+        idList = self.im.fetchall()
+        for i in idList:
+            idListe.append(i[0])
+        return idListe
 
-    def getProductsList(self) -> list:
-        self.checkDB()
-        return self.products
 
     
-
-
-    
-# self.im.execute("INSERT INTO products(productName,productURL,productTime) VALUES('rx590','deneme',15)")
-
 
 if __name__ == "__main__":
     pass
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from colorama import Fore, Style
 from msvcrt import getch
 from time import time
 
-MENUOPT = ["Check the prices of the product list","Add product to product list","Exit", "List product with id","Delete product with id","Add price manually with id","Get last price with id","Add stock manually with id","Get last stock state with id"]
+MENUOPT = ["Check the prices of the product list","Add product to product list","Exit","List product with id","Delete product with id","Add price manually with id","Get last price with id","Add stock manually with id","Get last stock state with id"]
 
 def printPriceTracker():
     f = Figlet(font="chunky")
@@ -40,8 +40,6 @@ if __name__ == "__main__":
                 else:
                     print(f"[ ] {i}")
 
-
-                 
             for i in detailText:
                 print(i)
 
@@ -66,7 +64,6 @@ if __name__ == "__main__":
                     selectedMenuOpt +=1
                 pressSpecialKey = False
         elif returnKey == b'\r':
-
             if selectedMenuOpt == 2:
                 exit()
             elif selectedMenuOpt == 6:
@@ -76,7 +73,6 @@ if __name__ == "__main__":
                     detailText.append(f"No product found with id {id}")
                 else:
                     detailText.append(f"ID: {myProduct.getId()} Price: {priceTracker.getLastPriceWithProductFromDb(myProduct)}")
-
             elif selectedMenuOpt == 7:
                 id = int(input("id"))
                 myProduct  = priceTracker.getProductFromDbWithId(id)
@@ -95,7 +91,6 @@ if __name__ == "__main__":
                     detailText.append(f"No product found with id {id}")
                 else:
                     detailText.append(f"ID: {myProduct.getId()} Stock: {priceTracker.getLastStockStateWithProductFromDb(myProduct)}")
-
             elif selectedMenuOpt == 5:
                 id = int(input("id"))
                 myProduct  = priceTracker.getProductFromDbWithId(id)
@@ -106,8 +101,6 @@ if __name__ == "__main__":
                     price = int(input("Price: "))
                     priceTracker.addPriceToDb(myProduct,price)
                     detailText.append(myProduct)
-                
-
             elif selectedMenuOpt == 3:
                 id = int(input("id"))
                 myProduct  = priceTracker.getProductFromDbWithId(id)
@@ -115,7 +108,6 @@ if __name__ == "__main__":
                     detailText.append(f"No product found with id {id}")
                 else:
                     detailText.append(myProduct)
-
             elif selectedMenuOpt == 4:
                 id = int(input("id"))
                 myProduct  = priceTracker.getProductFromDbWithId(id)
@@ -130,28 +122,21 @@ if __name__ == "__main__":
                         priceTracker.deleteProduct(myProduct)
                     else:
                         detailText.append("Deletion canceled")
-
             elif selectedMenuOpt == 1:
                 productURL = input("Product URL: ").strip()
                 #TODO ürünün URL bilgisinden hangi e-ticaret sitesinde olduğunu kontrol et
-                productURL = "htpps://www.fortesting.com/4541564654/41564654/564654654" # for testing
-                
                 productName = input("Product name: ").strip()
-                productName = "for testing "
-
                 priceTrack = input("Would you like to follow the price information of the product? (y/n)").strip()
                 if "y" in priceTrack or "Y" in priceTrack:
                     checkPrice = True
                 else:
                     checkPrice = False
-                    
                 stockTrack = input("Would you like to follow the stock information of the product? (y/n)").strip()
                 if "y" in stockTrack or "Y" in stockTrack:
                     checkStock = True
                 else:
                     checkStock = False
                 productTime = time()
-
                 try:
                     checkTime = input("How often would you like to be checked? (HH:MM:SS)(Ex: 00:10:00 every 10minute.) : ")
                     hour,minute,second = checkTime.split(":")
@@ -159,11 +144,21 @@ if __name__ == "__main__":
                 except:
                     totalSec = 600
                     print("Error! Default value 10 minute. You can change it in the product settings.")
-
                 product = Product(productName=productName,productURL=productURL,productTime=productTime,checkStock=checkStock,checkPrice=checkPrice,checkTime=totalSec)
                 productID = priceTracker.addProduct(product=product)
                 product.setId(productID)
                 detailText.append(f"The product named '{product.getProductName()}' has been added with the id number {product.getId()}.")
+            elif selectedMenuOpt == 0:
+                productLen = priceTracker.getDbLen()
+                if productLen > 0:
+                    system("cls")
+                    productList = priceTracker.getProductsList()
+                    for i in productList:
+                        print(i)
+                    input("....")
+                else:
+                    detailText.append("No products registered, add product")
+
 
 
 
